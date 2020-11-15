b96f79995315165f6cc62f793fa91f8d5f222046
Jordan Schneider <60559211+schneidercorn@users.noreply.github.com>
2020-11-14 22:02:15 -0500

    added project modals

---
+++ js/projects.js
@@ -0,0 +1,44 @@
+var modal = document.getElementsByClassName("projectModal");
+var button = document.getElementsByClassName("project");
+var span = document.getElementsByClassName("close");
+
+var totalProjects = 4;
+
+button[0].onclick = function() {
+  modal[0].style.display = "block";
+}
+button[1].onclick = function() {
+  modal[1].style.display = "block";
+}
+button[2].onclick = function() {
+  modal[2].style.display = "block";
+}
+button[3].onclick = function() {
+  modal[3].style.display = "block";
+}
+button[4].onclick = function() {
+  modal[4].style.display = "block";
+}
+
+
+span[0].onclick = function() {
+  modal[0].style.display = "none";
+}
+span[1].onclick = function() {
+  modal[1].style.display = "none";
+}
+span[2].onclick = function() {
+  modal[2].style.display = "none";
+}
+span[3].onclick = function() {
+  modal[3].style.display = "none";
+}
+span[4].onclick = function() {
+  modal[4].style.display = "none";
+}
+
+window.onclick = function(event) {
+  for (var i = 0; i < totalProjects; i++)
+    if (event.target == modal[i])
+      modal[i].style.display = "none";
+}
